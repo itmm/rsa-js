a4b2dfa61174426c2d1059cae79e076de1ff5c51
Timm Knape <timm.knape@adamhall.com>
2018-05-18 10:49:17 +0200

    refactoring

--- rsa.js
+++ rsa.js
@@ -12,48 +12,48 @@ jQuery(($) => {
 
 	let encrypt = true;
 
-	const gcd = (bn_a, bn_b) => {
-		let bn_ca = bn_a;
-		let bn_cb = bn_b;
+	const gcd = (a, b) => {
+		let ca = a;
+		let cb = b;
 
-		let bn_u = bigInt(1);
-		let bn_s = bigInt(0);
-		let bn_v = bn_s;
-		let bn_t = bn_u;
+		let u = bigInt.one;
+		let s = bigInt.zero;
+		let v = s;
+		let t = u;
 
 		let count = 0;
 
-		while (! bn_cb.isZero()) {
+		while (! cb.isZero()) {
 			if (++count >= 100) { alert("failed"); break; }
 
-			const dd = bn_ca.divmod(bn_cb);
-			const bn_na = bn_cb;
+			const dd = ca.divmod(cb);
+			const na = cb;
 
-			const bn_nb = dd.remainder;
-			const bn_nu = bn_s;
-			const bn_nv = bn_t;
-			const bn_ns = bn_u.subtract(dd.quotient.multiply(bn_s));
-			const bn_nt = bn_v.subtract(dd.quotient.multiply(bn_t));
+			const nb = dd.remainder;
+			const nu = s;
+			const nv = t;
+			const ns = u.subtract(dd.quotient.multiply(s));
+			const nt = v.subtract(dd.quotient.multiply(t));
 
-			bn_ca = bn_na;
-			bn_cb = bn_nb;
-			bn_u = bn_nu;
-			bn_v = bn_nv;
-			bn_s = bn_ns;
-			bn_t = bn_nt;
+			ca = na;
+			cb = nb;
+			u = nu;
+			v = nv;
+			s = ns;
+			t = nt;
 		}
-		return { a: bn_ca, u: bn_u, v: bn_v, s: bn_s, t: bn_t };
+		return { a: ca, u: u, v: v, s: s, t: t };
 	};
 
 	let timer;
 
 	const refresh = () => {
-		const bn_prime1 = bigInt($prime1.val());
-		const bn_prime2 = bigInt($prime2.val());
-		const public_key = bn_prime1.multiply(bn_prime2);
+		const prime1 = bigInt($prime1.val());
+		const prime2 = bigInt($prime2.val());
+		const public_key = prime1.multiply(prime2);
 		$public_key.text(public_key.toString());
 		const one = bigInt.one;
-		const phi = bn_prime1.subtract(one).multiply(bn_prime2.subtract(one));
+		const phi = prime1.subtract(one).multiply(prime2.subtract(one));
 		$phi.text(phi.toString());
 		const e = bigInt($e.val());
 		const gg = gcd(phi, e);
